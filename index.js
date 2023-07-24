3abcf4e1c48ab8e8ca6c1b8ecb79042996243638
Marco Dali <marco@durazno.io>
2023-07-23 22:19:54 -0600

    dynamic express routes completed

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express, { Router } from 'express'
+import { WebSocketServer } from 'ws';
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+let dynamicApiRouter = null
+// TODO: route_configs should be replaced by a redis database
+const route_configs = []
+const app = express()
 
-io.on("connection", (socket) => {
-  console.log(socket.id, 'has been connected');
+// Routes
+app.post('/socket', express.json(), (req, res) => {
+  route_configs.push({
+    method: 'get',
+    path: `/${req.body.username}/${req.body.project}`,
+    handler: (request, response) => {
+      response.json({ message: req.body.response })
+    }
+  })
+  console.log('route_configs', route_configs)
+  setupDynamicRouter()
+  res.status(201).json({ message: 'new route created successfully' })
+})
+app.delete('/socket', express.json(), (req, res) => {
+  // TODO: pending
+})
 
-  socket.on('disconnecting', () => {
-    console.log('anonymous is disconnecting', socket.rooms);
-  });
-});
+// Add routes to dynamicApiRouter from `route_configs`
+const setupDynamicRouter = () => {
+  dynamicApiRouter = new Router()
+  for (const config of route_configs) {
+    dynamicApiRouter[config.method](
+      config.path,
+      config.handler,
+    )
+  }
+}
 
-server.listen(process.env.PORT || 5000, () => {
-  console.log(`listening on http://localhost:${process.env.PORT || 5000}`);
-}); 
+// Dynamic routes are handled here
+app.use('/ws', (req, res, next) => dynamicApiRouter(req, res, next))
+
+// Error handling middleware
+app.use((req, res, next) => {
+  const err = new Error('Not found')
+  err.status = 404
+  next(err)
+})
+
+// Error handling response
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message
+    }
+  })
+})
+
+app.listen(process.env.PORT || 3000, err => {
+  if (err) console.error(err)
+  console.log("Server ready")
+})
