1b91462afe92a76a8a46fdeeee0af6bc01bd951c
Marco Dali <marco@durazno.io>
2023-09-10 06:36:23 -0600

    socket subdomain instead of api subdomain

--- clients/ephemeral.client.js
+++ clients/ephemeral.client.js
@@ -1,14 +1,14 @@
 import WebSocket from 'ws'
 
-const ws = new WebSocket(`ws://localhost:2000/whiteswan566/muffin`)
-//const ws = new WebSocket(`wss://api.mirna.cloud/goldencat269/fishhead`)
+//const ws = new WebSocket(`ws://localhost:2000/whiteswan566/muffin`)
+const ws = new WebSocket(`wss://socket.mirna.cloud/purplemeercat833/5551212`)
 
 ws.on('error', console.error)
 
 ws.on('open', () => {
-  ws.send('soy aquel que esperabas?')
+  ws.send('soy cox que esperabas?')
 });
 
 ws.on('message', (data) => {
-  console.log('received: %s', data)
+  console.log('recibido pareja cambio: %s', data)
 })
